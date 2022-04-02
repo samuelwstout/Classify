63071af8055d291983607c1f6099b513411a097e
samuelwstout <samuelwstout@gmail.com>
2022-04-02 06:03:40 -0500

    Add pseudo element MUI

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,16 +2,27 @@ import {useEffect} from 'react'
 import { styled } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import {useNavigate} from 'react-router-dom'
-
-
+import { hover } from '@testing-library/user-event/dist/hover'
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a45eb12484d24c4199050bdefee6d24b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
-const LoginButton = styled(Button)({
-  backgroundColor: 'green',
+const MainDiv = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh'
 })
+
+const LoginButton = styled(Button)({
+  backgroundColor: '#f2f2f2',
+  ':hover': {
+    backgroundColor: 'green'
+  }
+})
+
 const LoginButtonText = styled('a')({
-  color: 'white',
+  color: '#000',
+  textDecoration: 'none',
 })
 
 export const Login = ({ code }) => {
@@ -24,10 +35,10 @@ export const Login = ({ code }) => {
   }, [])
 
   return (
-    <div>
+    <MainDiv>
         <LoginButton variant='contained'>
             <LoginButtonText href={AUTH_URL}>Login to Spotify</LoginButtonText>
         </LoginButton>
-    </div>
+    </MainDiv>
   )
 }
