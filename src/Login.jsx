44d6d4560b1174b337072c09c719541f61507a60
samuelwstout <samuelwstout@gmail.com>
2022-04-04 13:49:26 -0500

    MUI media query success

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import {useNavigate} from 'react-router-dom'
 
+
 const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a45eb12484d24c4199050bdefee6d24b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 const MainDiv = styled('div')({
@@ -16,8 +17,11 @@ const LoginButton = styled(Button)({
   ':hover': {
     backgroundColor: '#000'
   },
+  '@media (max-width: 600px)': {
+    backgroundColor: 'red'
+  },
   width: '21rem',
-  height: '7rem'
+  height: '7rem',
 })
 const LoginButtonText = styled('a')({
   color: '#fff',
@@ -30,7 +34,7 @@ const LoginButtonText = styled('a')({
 const SpotifyLogo = styled('img')({
   width: '12rem',
   position: 'relative',
-  top: '1.3rem',
+  top: '1.35rem',
   left: '.8rem'
 })
 
@@ -44,10 +48,12 @@ export const Login = ({ code }) => {
   }, [])
 
   return (
+    <div>
     <MainDiv>
         <LoginButton variant='contained'>
             <LoginButtonText href={AUTH_URL}>LOGIN WITH<SpotifyLogo src='/spotify-icons-logos/logos/01_RGB/02_PNG/Spotify_Logo_RGB_Green.png'/></LoginButtonText>
         </LoginButton>
     </MainDiv>
+    </div>
   )
 }
