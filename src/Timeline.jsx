f5900f0b5278bb11deb7fb025877d2e54236d6f0
samuelwstout <samuelwstout@gmail.com>
2022-04-19 14:46:49 -0500

    No header/search

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -9,7 +9,7 @@ const Composer = styled('div')({
     alignItems: 'center',
     position: 'absolute',
     left: '20rem',
-    top: '8rem',
+    top: '1rem',
 })
 const Header = styled('div')({
     height: '6rem',
@@ -31,34 +31,35 @@ const TimelineDiv = styled('div')({
 
 export const Timeline = ({composers}) => {
 
-    const [color, setColor] = useState('white')
+    // const [color, setColor] = useState('white')
 
-    const handleInput = (e) => {
-        if (e) {
-           setColor('transparent')
-        }
-        if (e.target.value === '') {
-            setColor('white')
-        }
-        // console.log(e.target.value)
-        composers.map(composer => console.log(composer.props.children.props.children.props.children)) //These are all the composer names
-    }
-    const handleClick = (e) => {
-        if (e) {
-            setColor('white')
-        }
-    }
+    // const handleInput = (e) => {
+    //     if (e) {
+    //        setColor('transparent')
+    //     }
+    //     if (e.target.value === '') {
+    //         setColor('white')
+    //     }
+    //     const composerName = composers.map(composer => composer.props.children.props.children.props.children)
+    //     const value = e.target.value
+        
+    // }
+    // const handleClick = (e) => {
+    //     if (e) {
+    //         setColor('white')
+    //     }
+    // }
     return (
     <div>
-    <Header>
-        <form className="search-bar">
+    {/* <Header> */}
+        {/* <form className="search-bar">
                 <input onInput={handleInput} id="textsearch" type='text' title="Search"></input>
                 <img className="search-icon" src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/30/000000/external-search-logistic-delivery-kiranshastry-lineal-kiranshastry.png"></img>
                 <svg id="close-icon" role="button" height="24" width="24" viewBox="0 0 24 24"><path d="M3.293 3.293a1 1 0 011.414 0L12 10.586l7.293-7.293a1 1 0 111.414 1.414L13.414 12l7.293 7.293a1 1 0 01-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 01-1.414-1.414L10.586 12 3.293 4.707a1 1 0 010-1.414z"></path></svg>
                 <input onClick={handleClick} style={{backgroundColor: color}} id="resetbutton" type="reset" value=""></input>
                 <input className="searchbutton" type="submit" value="Search"></input>
-        </form>
-    </Header>
+        </form> */}
+    {/* </Header> */}
     <TimelineDiv>
         <div className='timeline'>
             <h3 className='date1400'>1400</h3>
@@ -78,3 +79,4 @@ export const Timeline = ({composers}) => {
     }
 
 export default Timeline;
+ 
